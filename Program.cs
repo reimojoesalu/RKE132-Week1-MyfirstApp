865e2db8e44645c5e233d0de7678252b0352a087
reimo.joesalu <reimo.joesalu@tktk.ee>
2024-03-19 16:44:43 +0200

    creat new app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//This is a comment to my code
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name"); //Output
+//string - sõne
+string userName = Console.ReadLine(); //Input
+
+userName = Console.ReadLine(); //Output
+
+Console.WriteLine("Hello" + "," + userName + "!");
+
+//string interpolation
+Console.WriteLine($"Hello, {userName} !");
